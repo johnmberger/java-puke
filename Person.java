9122827e40e62da5ed21fe6ebaefb5ce9dbcaa8b
johnmberger <johnmartinberger@gmail.com>
2016-10-31 11:16:12 -0600

    added more props to Person

--- Person.java
+++ Person.java
@@ -1,18 +1,24 @@
 public class Person {
 
   public String name;
+  public int age;
 
-  public Person(String name) {
+  public Person(String name, int age) {
     this.name = name;
+    this.age = age;
   }
 
   public String name() {
     return this.name;
   }
 
+  public int age() {
+    return this.age;
+  }
+
   public static void main(String[] args) {
-    Person student = new Person("John");
-    System.out.println(student.name());
+    Person student = new Person("John", 25);
+    System.out.println(student.name() + " is " + student.age() + " years old");
   }
 
 }
